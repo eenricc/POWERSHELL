9efb014edceb2fcd7cddf9c2f6767d2be76edd92
EenricC <eenricc@gmail.com>
2019-10-04 08:31:08 +0200

    Update OrganitzadorFotos-GUI.ps1

--- OrganitzadorFotos-GUI.ps1
+++ OrganitzadorFotos-GUI.ps1
@@ -75,7 +75,7 @@ $Button3.width                   = 400
 $Button3.height                  = 30
 $Button3.location                = New-Object System.Drawing.Point(34,374)
 $Button3.Font                    = 'Microsoft Sans Serif,10'
-$Button3.Add_Click({principal})
+$Button3.Add_Click({validaCarpeta})
 
 $Button4                         = New-Object system.Windows.Forms.Button
 $Button4.text                    = "Neteja"
@@ -102,6 +102,18 @@ Function clear_variable{
     Clear-Variable -name Origen, Desti, DestiOther, foo, date, arYear, arMonth, strYear, strMonth, mes, TargetPath, Files, File, titol, color, log, videos, MP4 -ErrorAction SilentlyContinue   
 }
 
+function validaCarpeta{
+    If ($TextBox1.textlength -eq 0 -or $TextBox2.textlength -eq 0){
+        [System.Windows.MessageBox]::Show('Algun dels camps està buit','ERROR','OK','Error')
+    }else{
+        If ((test-path $TextBox1.text) -and (test-path $TextBox2.text)){
+            principal
+        }else{
+            [System.Windows.MessageBox]::Show('Origen o destí no existeix','ERROR','OK','Error')
+        }
+    }
+}
+
 Function principal{
     $origen = $TextBox1.text
     $desti = $TextBox2.text
@@ -109,88 +121,81 @@ Function principal{
     $DestiOtherVideo = $DestiOther + "\Video"
     $DestiJPGissue = $DestiOther + "\JPG"
 
-    If ($TextBox1.textlength -eq 0 -or $TextBox2.textlength -eq 0){
-        [System.Windows.MessageBox]::Show('Algun dels camps està buit','ERROR','OK','Error')
-    }else{
-        If ((test-path $origen) -and (test-path $desti)){ 
-            $Dir = get-childitem $origen -recurse | Where-Object{!($_.PSIsContainer)}
-            $List = $Dir | where {$_.extension -ne ".jpg" -and $_.extension -ne ".mp4" -and $_.extension -ne ".avi"} 
-            $MP4 = $Dir | where {$_.extension -eq ".mp4" -or $_.extension -eq ".avi"}
-            $TextBox3.AppendText("ARXIUS NO .JPG`r`n")
-            $TextBox3.AppendText("----------------------------`r`n")
-            Foreach ($altres in $List){
-                If (Test-Path $DestiOther){
-                    Copy-Item -Path $altres.FullName -Destination $DestiOther
-                }Else{
-                    New-Item $DestiOther -Type Directory | Out-Null
-                    Copy-Item -Path $altres.FullName -Destination $DestiOther
-                }
-                $TextBox3.AppendText("$DestiOther\$altres`r`n")
-                $Global:ContadorAltres = $Global:ContadorAltres + 1
-            }
-            ForEach ($videos in $MP4){
-                If (Test-Path $DestiOtherVideo){
-                    Copy-Item -Path $videos.FullName -Destination $DestiOtherVideo
-                }Else{
-                    New-Item $DestiOtherVideo -Type Directory | Out-Null
-                    Copy-Item -Path $videos.FullName -Destination $DestiOtherVideo
-                }
-                $TextBox3.AppendText("$DestiOtherVideo\$videos`r`n")
-                $Global:ContadorAltres = $Global:ContadorAltres + 1
-            }
-          
-            $Files = Get-ChildItem -path $origen -recurse -filter *.jpg
-            $TextBox3.AppendText("`r`nARXIUS FORMAT .JPG`r`n")
-            $TextBox3.AppendText("--------------------------------------`r`n")
-            Foreach ($File in $Files){
-                $iserror = $false
-                $foo = New-Object -TypeName system.drawing.bitmap -ArgumentList $file.fullname
-                try {[void]$foo.GetPropertyItem(36867).value[0..9]}
-                catch {$iserror = $true}
-                If ($iserror -eq $true){ 
-                    New-Item $DestiJPGissue -Type Directory | Out-Null
-                    Copy-Item -Path $File.FullName -Destination $DestiJPGissue 
-                    $TextBox3.AppendText("$DestiJPGissue\$file`r`n")
-                    $Global:ContadorAltres = $Global:ContadorAltres + 1
-                }Else{
-                    $date = $foo.GetPropertyItem(36867).value[0..9]
-                    $arYear = [Char]$date[0],[Char]$date[1],[Char]$date[2],[Char]$date[3]
-                    $arMonth = [Char]$date[5],[Char]$date[6]
-                    $strYear = [String]::Join("",$arYear)
-                    $strMonth = [String]::Join("",$arMonth) 
-                    if ($strMonth -eq "01"){ $mes = "01 Gener" }
-                    if ($strMonth -eq "02"){ $mes = "02 Febrer" }
-                    if ($strMonth -eq "03"){ $mes = "03 Març" }
-                    if ($strMonth -eq "04"){ $mes = "04 Abril" }
-                    if ($strMonth -eq "05"){ $mes = "05 Maig" }
-                    if ($strMonth -eq "06"){ $mes = "06 Juny" }
-                    if ($strMonth -eq "07"){ $mes = "07 Juliol" }
-                    if ($strMonth -eq "08"){ $mes = "08 Agost" }
-                    if ($strMonth -eq "09"){ $mes = "09 Setembre" }
-                    if ($strMonth -eq "10"){ $mes = "10 Octubre" }
-                    if ($strMonth -eq "11"){ $mes = "11 Novembre" }
-                    if ($strMonth -eq "12"){ $mes = "12 Desembre" }
-                    $TargetPath = $Desti + "\" + $strYear + "\" + $mes
-                    $TextBox3.AppendText("$TargetPath\$file`r`n")
-                    If (Test-Path $TargetPath){
-                        Copy-Item -Path $file.FullName -Destination $TargetPath
-                    }Else{
-                        New-Item $TargetPath -Type Directory | Out-Null
-                        Copy-Item -Path $file.FullName -Destination $TargetPath
-                    }
-                    $Global:Contador = $Global:Contador + 1
-            
-                }
-            }
-            $TextBox3.AppendText("`r`n")
-            $TextBox3.AppendText("TOTAL ARXIUS`r`n")
-            $TextBox3.AppendText("--------------------------`r`n")
-            $TextBox3.AppendText("Arxius JPG Correctes: $Global:Contador`r`n")
-            $TextBox3.AppendText("Carpeta Altres: $Global:ContadorAltres`r`n")
-            $total = $Global:Contador + $Global:ContadorAltres
-            $TextBox3.AppendText("Total: $total`n")   
+    $Dir = get-childitem $origen -recurse | Where-Object{!($_.PSIsContainer)}
+    $List = $Dir | where {$_.extension -ne ".jpg" -and $_.extension -ne ".mp4" -and $_.extension -ne ".avi"} 
+    $MP4 = $Dir | where {$_.extension -eq ".mp4" -or $_.extension -eq ".avi"}
+    $TextBox3.AppendText("ARXIUS NO .JPG`r`n")
+    $TextBox3.AppendText("----------------------------`r`n")
+    Foreach ($altres in $List){
+        If (Test-Path $DestiOther){
+            Copy-Item -Path $altres.FullName -Destination $DestiOther
+        }Else{
+            New-Item $DestiOther -Type Directory | Out-Null
+            Copy-Item -Path $altres.FullName -Destination $DestiOther
         }
-    }           
+        $TextBox3.AppendText("$DestiOther\$altres`r`n")
+        $Global:ContadorAltres = $Global:ContadorAltres + 1
+    }
+    ForEach ($videos in $MP4){
+        If (Test-Path $DestiOtherVideo){
+            Copy-Item -Path $videos.FullName -Destination $DestiOtherVideo
+        }Else{
+            New-Item $DestiOtherVideo -Type Directory | Out-Null
+            Copy-Item -Path $videos.FullName -Destination $DestiOtherVideo
+        }
+        $TextBox3.AppendText("$DestiOtherVideo\$videos`r`n")
+        $Global:ContadorAltres = $Global:ContadorAltres + 1
+    }
+    
+    $Files = Get-ChildItem -path $origen -recurse -filter *.jpg
+    $TextBox3.AppendText("`r`nARXIUS FORMAT .JPG`r`n")
+    $TextBox3.AppendText("--------------------------------------`r`n")
+    Foreach ($File in $Files){
+        $iserror = $false
+        $foo = New-Object -TypeName system.drawing.bitmap -ArgumentList $file.fullname
+        try {[void]$foo.GetPropertyItem(36867).value[0..9]}
+        catch {$iserror = $true}
+        If ($iserror -eq $true){ 
+            New-Item $DestiJPGissue -Type Directory | Out-Null
+            Copy-Item -Path $File.FullName -Destination $DestiJPGissue 
+            $TextBox3.AppendText("$DestiJPGissue\$file`r`n")
+            $Global:ContadorAltres = $Global:ContadorAltres + 1
+        }Else{
+            $date = $foo.GetPropertyItem(36867).value[0..9]
+            $arYear = [Char]$date[0],[Char]$date[1],[Char]$date[2],[Char]$date[3]
+            $arMonth = [Char]$date[5],[Char]$date[6]
+            $strYear = [String]::Join("",$arYear)
+            $strMonth = [String]::Join("",$arMonth) 
+            if ($strMonth -eq "01"){ $mes = "01 Gener" }
+            if ($strMonth -eq "02"){ $mes = "02 Febrer" }
+            if ($strMonth -eq "03"){ $mes = "03 Març" }
+            if ($strMonth -eq "04"){ $mes = "04 Abril" }
+            if ($strMonth -eq "05"){ $mes = "05 Maig" }
+            if ($strMonth -eq "06"){ $mes = "06 Juny" }
+            if ($strMonth -eq "07"){ $mes = "07 Juliol" }
+            if ($strMonth -eq "08"){ $mes = "08 Agost" }
+            if ($strMonth -eq "09"){ $mes = "09 Setembre" }
+            if ($strMonth -eq "10"){ $mes = "10 Octubre" }
+            if ($strMonth -eq "11"){ $mes = "11 Novembre" }
+            if ($strMonth -eq "12"){ $mes = "12 Desembre" }
+            $TargetPath = $Desti + "\" + $strYear + "\" + $mes
+            $TextBox3.AppendText("$TargetPath\$file`r`n")
+            If (Test-Path $TargetPath){
+                Copy-Item -Path $file.FullName -Destination $TargetPath
+            }Else{
+                New-Item $TargetPath -Type Directory | Out-Null
+                Copy-Item -Path $file.FullName -Destination $TargetPath
+            }
+            $Global:Contador = $Global:Contador + 1
+        }
+    }
+    $TextBox3.AppendText("`r`n")
+    $TextBox3.AppendText("TOTAL ARXIUS`r`n")
+    $TextBox3.AppendText("--------------------------`r`n")
+    $TextBox3.AppendText("Arxius JPG Correctes: $Global:Contador`r`n")
+    $TextBox3.AppendText("Carpeta Altres: $Global:ContadorAltres`r`n")
+    $total = $Global:Contador + $Global:ContadorAltres
+    $TextBox3.AppendText("Total: $total`n")   
 }
 
 Function neteja{
